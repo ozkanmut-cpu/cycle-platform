d4e6d942ca6baba676e236b1f9f4e740cd53eeba
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 22:16:47 +0300

    feat(storage): export health sync cursor contract

--- packages/storage/lib/cycle_storage.dart
+++ packages/storage/lib/cycle_storage.dart
@@ -3,6 +3,7 @@ library cycle_storage;
 export 'src/audit_event.dart';
 export 'src/binary_vault.dart';
 export 'src/health_event_repository.dart';
+export 'src/health_sync_cursor.dart';
 export 'src/recovery_lifecycle.dart';
 export 'src/storage_transaction.dart';
 export 'src/vault_lifecycle.dart';
