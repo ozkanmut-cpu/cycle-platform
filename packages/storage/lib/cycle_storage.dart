07e9f2764a96e512be32a4c78f38badbdffa233d
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 19:04:26 +0300

    feat(storage): export recovery lifecycle

--- packages/storage/lib/cycle_storage.dart
+++ packages/storage/lib/cycle_storage.dart
@@ -3,6 +3,7 @@ library cycle_storage;
 export 'src/audit_event.dart';
 export 'src/binary_vault.dart';
 export 'src/health_event_repository.dart';
+export 'src/recovery_lifecycle.dart';
 export 'src/storage_transaction.dart';
 export 'src/vault_lifecycle.dart';
 export 'src/vault_record.dart';
