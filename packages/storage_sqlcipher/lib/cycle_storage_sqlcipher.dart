50119888c33302c7f03a4f86c2eef3369f8f35bb
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 18:04:53 +0300

    feat(storage): export vault lifecycle

--- packages/storage_sqlcipher/lib/cycle_storage_sqlcipher.dart
+++ packages/storage_sqlcipher/lib/cycle_storage_sqlcipher.dart
@@ -3,3 +3,4 @@ library cycle_storage_sqlcipher;
 export 'src/sqlcipher_audit_log_repository.dart';
 export 'src/sqlcipher_database.dart';
 export 'src/sqlcipher_health_event_repository.dart';
+export 'src/sqlcipher_vault_lifecycle.dart';
