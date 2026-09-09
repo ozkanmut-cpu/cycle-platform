f52f394f4a2394eee23102e24b2a12cb37b666d5
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 18:20:47 +0300

    style: normalize secure key store formatting

--- packages/secure_key_store/lib/src/flutter_secure_key_store.dart
+++ packages/secure_key_store/lib/src/flutter_secure_key_store.dart
@@ -6,7 +6,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class FlutterSecureKeyStore implements SecureKeyStore {
   FlutterSecureKeyStore({FlutterSecureStorage? storage})
-    : _storage = storage ?? const FlutterSecureStorage();
+      : _storage = storage ?? const FlutterSecureStorage();
 
   final FlutterSecureStorage _storage;
 
