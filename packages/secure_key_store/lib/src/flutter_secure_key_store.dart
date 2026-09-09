bebfa78f758ec748b1ce7224ecaa78d699200c27
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 18:55:15 +0300

    security: harden platform secure storage defaults

--- packages/secure_key_store/lib/src/flutter_secure_key_store.dart
+++ packages/secure_key_store/lib/src/flutter_secure_key_store.dart
@@ -6,7 +6,14 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class FlutterSecureKeyStore implements SecureKeyStore {
   FlutterSecureKeyStore({FlutterSecureStorage? storage})
-      : _storage = storage ?? const FlutterSecureStorage();
+      : _storage =
+            storage ??
+            const FlutterSecureStorage(
+              aOptions: AndroidOptions(),
+              iOptions: IOSOptions(
+                accessibility: KeychainAccessibility.unlocked_this_device,
+              ),
+            );
 
   final FlutterSecureStorage _storage;
 
