94ebda68cf1c9ebe25e34f6263fe8d1717f64e96
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 18:38:44 +0300

    feat(storage): export binary vault contracts

--- packages/storage/lib/cycle_storage.dart
+++ packages/storage/lib/cycle_storage.dart
@@ -1,6 +1,7 @@
 library cycle_storage;
 
 export 'src/audit_event.dart';
+export 'src/binary_vault.dart';
 export 'src/health_event_repository.dart';
 export 'src/storage_transaction.dart';
 export 'src/vault_lifecycle.dart';
