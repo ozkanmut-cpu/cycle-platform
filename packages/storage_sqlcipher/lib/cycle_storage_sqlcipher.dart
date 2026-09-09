f517bf40ba43fb512659a8c36f43bb4282165e3a
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 22:23:10 +0300

    feat(storage): export atomic health import committer

--- packages/storage_sqlcipher/lib/cycle_storage_sqlcipher.dart
+++ packages/storage_sqlcipher/lib/cycle_storage_sqlcipher.dart
@@ -3,5 +3,6 @@ library cycle_storage_sqlcipher;
 export 'src/sqlcipher_audit_log_repository.dart';
 export 'src/sqlcipher_database.dart';
 export 'src/sqlcipher_health_event_repository.dart';
+export 'src/sqlcipher_health_import_committer.dart';
 export 'src/sqlcipher_health_sync_cursor_repository.dart';
 export 'src/sqlcipher_vault_lifecycle.dart';
