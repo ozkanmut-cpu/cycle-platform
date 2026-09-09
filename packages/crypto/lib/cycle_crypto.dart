107994ec348f6d8ed1111cbb7ec3dae50cfdcca9
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 18:45:19 +0300

    feat(crypto): export backup key envelope

--- packages/crypto/lib/cycle_crypto.dart
+++ packages/crypto/lib/cycle_crypto.dart
@@ -2,6 +2,7 @@ library cycle_crypto;
 
 export 'src/aes_gcm_authenticated_cipher.dart';
 export 'src/authenticated_cipher.dart';
+export 'src/backup_key_envelope.dart';
 export 'src/cryptographic_erase.dart';
 export 'src/key_deriver.dart';
 export 'src/key_envelope.dart';
