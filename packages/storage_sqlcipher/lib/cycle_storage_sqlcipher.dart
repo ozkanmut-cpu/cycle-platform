64dc2d3c79bc03a3a4a42b7ab3e5f4a032c36d09
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-09 22:17:25 +0300

    feat(storage): export SQLCipher sync cursor repository

--- packages/storage_sqlcipher/lib/cycle_storage_sqlcipher.dart
+++ packages/storage_sqlcipher/lib/cycle_storage_sqlcipher.dart
@@ -3,4 +3,5 @@ library cycle_storage_sqlcipher;
 export 'src/sqlcipher_audit_log_repository.dart';
 export 'src/sqlcipher_database.dart';
 export 'src/sqlcipher_health_event_repository.dart';
+export 'src/sqlcipher_health_sync_cursor_repository.dart';
 export 'src/sqlcipher_vault_lifecycle.dart';
