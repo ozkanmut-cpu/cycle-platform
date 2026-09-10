74d1f1d705c704f39ec4d14cef26aa82999def3b
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-10 03:31:45 +0300

    feat(storage): export health import history repository

--- packages/storage_sqlcipher/lib/cycle_storage_sqlcipher.dart
+++ packages/storage_sqlcipher/lib/cycle_storage_sqlcipher.dart
@@ -5,5 +5,6 @@ export 'src/sqlcipher_audit_log_repository.dart';
 export 'src/sqlcipher_database.dart';
 export 'src/sqlcipher_health_event_repository.dart';
 export 'src/sqlcipher_health_import_committer.dart';
+export 'src/sqlcipher_health_import_history_repository.dart';
 export 'src/sqlcipher_health_sync_cursor_repository.dart';
 export 'src/sqlcipher_vault_lifecycle.dart';
