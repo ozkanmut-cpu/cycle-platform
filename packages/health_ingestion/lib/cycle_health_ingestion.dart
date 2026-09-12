a79da6d48284e9f55e01b89b21972b0247e892e1
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-12 18:45:52 +0300

    feat(health): export missing bucket aggregation

--- packages/health_ingestion/lib/cycle_health_ingestion.dart
+++ packages/health_ingestion/lib/cycle_health_ingestion.dart
@@ -3,6 +3,7 @@ library cycle_health_ingestion;
 export 'src/health_ingestion.dart';
 export 'src/health_sync.dart';
 export 'src/import_coordinator.dart';
+export 'src/missing_bucket_aggregation.dart';
 export 'src/near_duplicate.dart';
 export 'src/platform_adapters.dart';
 export 'src/raw_sensor_payload.dart';
