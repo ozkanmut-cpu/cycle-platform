dad0e04ada27de4153f1e892f18df5fc6cba48ae
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-13 10:25:51 +0300

    feat(clinical): export expanded condition catalog

--- packages/clinical_domain/lib/cycle_clinical_domain.dart
+++ packages/clinical_domain/lib/cycle_clinical_domain.dart
@@ -1,6 +1,7 @@
 library cycle_clinical_domain;
 
 export 'src/condition_catalog.dart';
+export 'src/condition_catalog_expansion.dart';
 export 'src/condition_catalog_definitions.dart';
 export 'src/condition_pack.dart';
 export 'src/rule_validation.dart';
